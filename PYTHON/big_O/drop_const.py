4b1a6eaba8d3a6eab62d8f32fe2f1df724e79024
Username <albavmolinanyc@gmail.com>
2022-05-21 19:26:06 -0400

    adds more topics

---
+++ PYTHON/big_O/drop_const.py
@@ -0,0 +1,10 @@
+def print_items(n):
+    for i in range(n):
+        print(i)
+    
+    for j in range(n):
+        print(j)
+
+print_items(10)
+
+# first rule of simplify drop constants
---
+++ PYTHON/big_O/o(n)^2.py
@@ -0,0 +1,20 @@
+# Ex 1
+def print_items(n): 
+    for i in range(n):
+        for j in range(n):
+                       print(i,j)
+
+
+
+print_items(10)
+
+# Ex 2 -- adds another 
+def print_items(n): 
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                       print(i,j,k)
+
+
+
+print_items(10)
