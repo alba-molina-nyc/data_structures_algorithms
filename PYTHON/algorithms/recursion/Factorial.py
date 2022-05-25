27fab8aaf61c85afce8d2bc1aa8cedca1b475fdf
Username <albavmolinanyc@gmail.com>
2022-05-24 21:02:27 -0400

    facotiral

--- PYTHON/algorithms/recursion/Factorial.py
+++ PYTHON/algorithms/recursion/Factorial.py
@@ -10,11 +10,20 @@ def factorial(n):
 
 print(factorial(4))
 
-# so in the above example we are setting n = 4 
-# so then it says is 4 = 1 no
-# so then it will call the function again n * factoria(n-1)
-# so then it will say 3 = 1 no
-# until you get to 1 and then return 1 
+# look at factorial vid again
+# looking back at the call stack.py in this folder you can apply the same logic to this example
+# you call factorial(4) and that gets added to the very bottom of the call stack
+# then factorial(3) gets added next in the call stack
+# then factorial(2) gets added next in the call stack
+# then factorial(1) gets added LASTLY for this example in the call stack
+# --
+# then you go through the call stack and stacks start at the top so it would go
+# factorial(1)
+# factorial(2)
+# factorial(3)
+# factorial(4) until we get to 24 which is the number we return 
+
+
 
 # def factorial(n):
 #     if n == 1:
